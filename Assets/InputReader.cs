11ead8784b56297ed9b0926a52aa9fdcd970ba2e
Raj Gohil <80092049+rajgohil28@users.noreply.github.com>
2022-03-15 17:13:53 +0530

    First Commit

---
+++ Assets/InputReader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputReader : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Debug.Log("Horizontal :" + horizontal + "Vertial :" + vertical);
+    }
+}
---
+++ Assets/TriggerExtinguisherScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class TriggerExtinguisherScript : MonoBehaviour
+{
+    private Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            FireRayCast();
+        }
+    }
+    void FireRayCast()
+    {
+        Ray ray = cam.ScreenPointToRay(new Vector2(Screen.width/2, Screen.height/2));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 10000))
+        {
+            Debug.Log(hit.transform.gameObject.name);
+        }
+    }
+}
